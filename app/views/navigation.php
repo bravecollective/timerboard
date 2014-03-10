8a248a6f0bcd4d3378224a71a48a5cf6438e0a00
Matthew Glinski <matthewglinski@gmail.com>
2014-03-10 01:53:31 -0400

    Commit of v1.0

--- app/views/navigation.php
+++ app/views/navigation.php
@@ -21,7 +21,7 @@
 				{
 					?>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, <strong><?=Auth::user()->username?></strong> <b class="caret"></b></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, <strong><?=Auth::user()->character_name?></strong> <b class="caret"></b></a>
 						<ul class="dropdown-menu">
 							<li><a href="<?=URL::route('logout')?>">Logout</a></li>
 						</ul>
