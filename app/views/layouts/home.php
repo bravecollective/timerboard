fda37975cd7a3133193ebe8f4a487a4353668460
Matthew Glinski <matthewglinski@gmail.com>
2014-03-10 01:00:04 -0400

    Commit of v1.0

--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>BNI Timers</title>
+    <title>HERO Timers</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
--- app/views/layouts/login.php
+++ app/views/layouts/login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Login - BNI Timers</title>
+	<title>Login - HERO Timers</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
