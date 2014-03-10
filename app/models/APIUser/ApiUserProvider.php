c446b530765523e649a38f06ddcdc1619caa2d74
Matthew Glinski <matthewglinski@gmail.com>
2014-03-10 00:52:28 -0400

    Commit of v1.0

--- app/models/APIUser/ApiUserProvider.php
+++ app/models/APIUser/ApiUserProvider.php
@@ -12,12 +12,35 @@ class ApiUserProvider implements UserProviderInterface {
 
 	public function retrieveByCredentials(array $credentials)
 	{
-		$user = ApiUser::where('token', '=', $credentials['token'])->get();
-		if(isset($user[0]))
+		try
 		{
-			return $user[0];
+
+			$user = ApiUser::where('token', '=', $credentials['token'])->get();
+			if(isset($user[0]))
+			{
+				return $user[0];
+			}
+			else
+			{
+				$api = new Brave\API(Config::get('braveapi.application-endpoint'), Config::get('braveapi.application-identifier'), Config::get('braveapi.local-private-key'), Config::get('braveapi.remote-public-key'));
+				$result = $api->core->info(array('token' => $credentials['token']));
+
+				//
+				if(!isset($result->character->name))
+				{
+					return Redirect::route('logintest')
+					               ->with('flash_error', 'Login Failed, Please Try Again');
+				}
+
+				//
+				$user = $this->updateUser($credentials['token'], $result);
+				return $user;
+			}
 		}
-		return $user[0];
+		catch(Exception $e)
+		{
+			return false;
+		}
 	}
 
 	public function validateCredentials(UserInterface $user, array $credentials)
@@ -32,54 +55,13 @@ class ApiUserProvider implements UserProviderInterface {
 			$api = new Brave\API(Config::get('braveapi.application-endpoint'), Config::get('braveapi.application-identifier'), Config::get('braveapi.local-private-key'), Config::get('braveapi.remote-public-key'));
 			$result = $api->core->info(array('token' => $credentials['token']));
 
-			//
 			if(!isset($result->character->name))
 			{
 				return Redirect::route('logintest')
-					->with('flash_error', 'Login Failed, Please Try Again');
+				               ->with('flash_error', 'Login Failed, Please Try Again');
 			}
 
-			// validate permissions
-			$permission = 0;
-			foreach(Config::get('braveapi.auth-edit-tags') as $tag)
-			{
-				if(in_array($tag, $result->tags) and $permission == 0) // check for special group
-				{
-					$permission = 1;
-				}
-			}
-
-			// check for existing user
-			$userfound = ApiUser::find($result->character->id);
-			if($userfound == false)
-			{
-				// no user found, create it
-				ApiUser::create(array(
-	                'id' => $result->character->id,
-	                'token' => $credentials['token'],
-	                'character_name' => $result->character->name,
-	                'alliance_id' => $result->alliance->id,
-	                'alliance_name' => $result->alliance->name,
-	                'tags' => json_encode($result->tags),
-	                'status' => 1,
-	                'permission' => $permission
-                ));
-			}
-			else
-			{
-				// update the existing user
-				$userfound->token = $credentials['token'];
-				$userfound->status = 1;
-				$userfound->permission = $permission;
-				$userfound->token = $credentials['token'];
-                $userfound->character_name = $result->character->name;
-                $userfound->alliance_id = $result->alliance->id;
-                $userfound->alliance_name = $result->alliance->name;
-                $userfound->tags = json_encode($result->tags);
-
-				$userfound->save();
-			}
-
+			$this->updateUser($credentials['token'], $result);
 			return true;
 		}
 		catch(Exception $e)
@@ -87,5 +69,51 @@ class ApiUserProvider implements UserProviderInterface {
 			return Redirect::route('logintest')
 				->with('flash_error', 'Login Failed, Please Try Again');
 		}
+	}
+
+	private function updateUser($token, $result)
+	{
+		// validate permissions
+		$permission = 0;
+		foreach(Config::get('braveapi.auth-edit-tags') as $tag)
+		{
+			if(in_array($tag, $result->tags) and $permission == 0) // check for special group
+			{
+				$permission = 1;
+			}
+		}
+
+		// check for existing user
+		$userfound = ApiUser::find($result->character->id);
+		if($userfound == false)
+		{
+			// no user found, create it
+			$userfound = ApiUser::create(array(
+				                'id' => $result->character->id,
+				                'token' => $token,
+				                'character_name' => $result->character->name,
+				                'alliance_id' => $result->alliance->id,
+				                'alliance_name' => $result->alliance->name,
+				                'tags' => json_encode($result->tags),
+				                'status' => 1,
+				                'permission' => $permission
+			                ));
+		}
+		else
+		{
+			// update the existing user
+			$userfound->token = $token;
+			$userfound->status = 1;
+			$userfound->permission = $permission;
+			$userfound->token = $token;
+			$userfound->character_name = $result->character->name;
+			$userfound->alliance_id = $result->alliance->id;
+			$userfound->alliance_name = $result->alliance->name;
+			$userfound->tags = json_encode($result->tags);
+
+			$userfound->save();
+		}
+
+		return $userfound;
 	}
 }
